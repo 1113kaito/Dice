e4bbff3dd4a2e4056bfbfa6ad6ad0ca2de333bc0
s1300149 <s1300149@sshsv212.u-aizu.ac.jp>
2023-06-15 00:57:08 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -9,5 +9,10 @@ class DiceGame{
         System.out.println("Die 1:"+rani1);
         System.out.println("Die 2:"+rani2);
         System.out.println("Total value:"+(rani1+rani2));
+	 if(rani1+rani2 >=7){
+            System.out.println("You won");
+        }else{
+            System.out.println("You lost");
+        }
     }
 }
