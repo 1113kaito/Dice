4a93e0a884a80905cf7f130d31002b4c7a300f41
s1300149 <s1300149@sshsv212.u-aizu.ac.jp>
2023-06-15 00:44:21 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+class DiceGame{
+    public static void main(String[] args){
+        Random ran = new Random();
+        int rani1 = ran.nextInt(5)+1;
+        int rani2 = ran.nextInt(5)+1;
+        System.out.println("Rolling dice...");
+        System.out.println("Die 1:"+rani1);
+        System.out.println("Die 2:"+rani2);
+        System.out.println("Total value:"+(rani1+rani2));
+    }
+}
